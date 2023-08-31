ac4cbd5829bde8132239421ad5bb1d6f85a89498
Ruth97 <122542861+Ruth97@users.noreply.github.com>
2023-08-30 21:24:08 -0300

    Add files via upload

---
+++ sintaxe/variaveis/e/fluxos/PontoFlutuante.java
@@ -0,0 +1,15 @@
+package sintaxe.variaveis.e.fluxos;
+
+// Declarando a variável Double em Java
+
+public class PontoFlutuante {
+	public static void main (String[] args)
+	{
+		double salario = 2.300; // Interessante já declarar a variável e dar um nome a ela em seguida + o valor dela.
+		System.out.println("Meu salario eh de " + salario + "k/reais");
+		
+		double calculando = 3.5 * 79; // É possível tbm já lançar cálculos 
+		System.out.println(calculando);
+	}
+
+}
---
+++ sintaxe/variaveis/e/fluxos/TestaVariaveis.java
@@ -0,0 +1,18 @@
+package sintaxe.variaveis.e.fluxos;
+
+public class TestaVariaveis {
+	public static void main (String[] args) {
+		System.out.println("Ola Ruth, este é seu novo teste!\n Você tem: ");
+		
+		int idade;
+		idade = 26;
+		
+		System.out.println("Sua idade eh " + idade + " anos, certo?"); // Colocar o + para fazer junção de string e outros tipos de variáveis
+		
+		idade = 26 + 4;
+		
+		System.out.println(idade);
+
+	}
+
+}
